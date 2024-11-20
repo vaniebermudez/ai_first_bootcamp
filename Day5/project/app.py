8d0e9119e4a5efb484052b45463b4ec6c7786910
Vanie Bermudez <55613032+vaniebermudez@users.noreply.github.com>
2024-11-20 18:25:02 +0800

    Update app.py

--- Day5/project/app.py
+++ Day5/project/app.py
@@ -126,7 +126,7 @@ with st.sidebar:
     options = option_menu(
         "Dashboard", 
         ["Home", "About Me", "SmartSellAI"],
-        icons = ['book', 'man', 'tools'],
+        icons = ['book', 'globe', 'tools'],
         menu_icon = "book", 
         default_index = 0,
         styles = {
